82f5f22f2edd4f773e929ce55e879d1b9c3de628
penguin-san <itosouplus@gmail.com>
2016-02-03 10:09:16 +0900

    commit

---
+++ RSSReader/CharactorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CharactorViewController.swift
+//  Curation
+//
+//  Created by 伊藤総一郎 on 2/2/16.
+//  Copyright © 2016 susieyy. All rights reserved.
+//
+
+import UIKit
+
+class CharactorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
